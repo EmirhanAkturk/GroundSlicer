2b37a171610aaf735491dbe3ec109bbfeb9a47f5
FatihGayberi <fatihgayberii@gmail.com>
2020-12-08 00:54:48 +0300

    MoveController.cs edited

--- GroundSlicer/Assets/Scripts/MoveController.cs
+++ GroundSlicer/Assets/Scripts/MoveController.cs
@@ -2,59 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MoveController : MonoBehaviour
 {
     [SerializeField] Rigidbody rbCharacter;
-    [SerializeField] GameObject target;
+    //[SerializeField] GameObject target;
     [SerializeField] float speed;
-    [SerializeField] float speedModifier;
-    bool isMove;
+    //[SerializeField] float speedModifier;
+    float vX0, vX1;
+    float vY0, vY1;
+    float m2;
+    Vector2 firstPos;
+    Vector2 lastPos;
+    //bool isMove;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        vY0 = speed;
     }
 
     void Update()
     {
-        CharacterMove();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
         TouchControl();
-    }
-
-
-    void CharacterMove()
-    {
-        if (isMove)
-        {
-            rbCharacter.velocity = new Vector3(0f, 0f, speed);
-        }
     }
 
     void TouchControl()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            vX1 = vX0;
+            vY1 = vY0;
+            rbCharacter.velocity = new Vector3(vX0, 0, vY0);
+
+            firstPos = Input.mousePosition;
+        }
         if (Input.GetMouseButton(0))
         {
-            isMove = true;
-            transform.LookAt(target.transform.position);
-            TargetPositionUpdate();
+            lastPos = Input.mousePosition;
+
+            Debug.Log("*****" + firstPos.x + " - " + firstPos.y + "*****");
+            Debug.Log(Input.mousePosition.x + " - " + Input.mousePosition.y);
+            if (firstPos != lastPos)
+            {
+                m2 = (lastPos.y - firstPos.y) / (lastPos.x - firstPos.x);
+                vX1 = speed / Mathf.Sqrt(1 + m2 * m2);
+                vX1 = Mathf.Abs(vX1);
+                if (lastPos.x < firstPos.x)
+                {
+                    vX1 *= -1;
+                }
+                vY1 = m2 * vX1;
+
+                rbCharacter.velocity = new Vector3(vX1, 0, vY1);
+            }
         }
         if (Input.GetMouseButtonUp(0))
         {
+            vX0 = vX1;
+            vY0 = vY1;
             // agir cekim
         }
-    }
-
-    void TargetPositionUpdate()
-    {
-        float h = speedModifier * Input.GetAxis("Mouse X");
-        float v = speedModifier * Input.GetAxis("Mouse Y");
-
-        target.transform.position = new Vector3(transform.position.x + h, transform.position.y, transform.position.z + v);
     }
 }
