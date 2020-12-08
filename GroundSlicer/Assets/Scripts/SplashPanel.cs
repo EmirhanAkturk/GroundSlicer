57b0651bef355e6778056913b707b698916225ab
FatihGayberi <fatihgayberii@gmail.com>
2020-12-08 22:47:09 +0300

    FirstOpen video added

---
+++ GroundSlicer/Assets/Scripts/SplashPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashPanel : MonoBehaviour
+{
+    public void GoButton()
+    {
+        PlayerPrefs.SetInt("FirstOpen", 1);
+        gameObject.SetActive(false);
+    }
+}
---
+++ GroundSlicer/Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject splashPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SplashControl();
+    }
+
+    void SplashControl()
+    {
+        if (PlayerPrefs.GetInt("FirstOpen") == 0)
+        {
+            splashPanel.SetActive(true);
+        }
+    }
+}
